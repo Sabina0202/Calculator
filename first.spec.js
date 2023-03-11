d7770db59e63a4fbfb16e87e16c017fbb0bcca58
Sabina <sabisha2015@list.ru>
2023-03-11 15:47:30 +0300

    Add files via upload

---
+++ first.spec.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect
+
+    describe('First suit', function () {
+
+        it('first test', async function () {
+            await browser.get('https://b2c.pampadu.ru/index.html#49a973bd-2d7c-4b9b-9c28-d986d7757983')
+        })
+    })
+
